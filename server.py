122a0747a7dfc9efb31f73e41898cd7cdd025c88
4nar <anar.abetayeva@gmail.com>
2016-02-06 21:34:22 +0600

    task number 1 first commit

--- server.py
+++ server.py
@@ -3,10 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 import os 
 
-
+url = "http://public.mig.kz/"
 @route('/')
 def index():
-    return {"usd": 378, "eur": 415, "rub": 4.82}
+    # return {"usd": 378, "eur": 415, "rub": 4.82}
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+
+
+    l1 = []
+    l2 = []
+    for tag in soup.find_all('ul'):
+        if tag.has_attr('class') and 'clearfix' in tag.attrs['class'] and len(tag.attrs['class'])==1:
+
+        	l1 = tag.find_all('h4')
+        	l2 = tag.find_all('p')
+        	for i in range(0,len(l1)):
+        		l1[i]=l1[i].text
+        		l2[i]=float(l2[i].text.split()[0])
+
+    dic = dict(zip(l1,l2))
+    return dic     	
+
+ 
+    	
 
 
 URL = "https://github.com/giAtSDU/apt_spring_2016_hw1"
